c4e7dfe713f19f925acd9330bafc3ffc9a8ed011
pbrehill <pbrehill@gmail.com>
2023-11-16 18:00:33 +1100

    Working on data import

--- Data set-up.R
+++ Data set-up.R
@@ -8,6 +8,12 @@ library(car)
 library(cowplot)
 library(stargazer)
 # library(lm.beta)
+source("helpers.R")
+
+covariates <- c('hhh_gender', 'program_knowledge', 'hhh_age', 'monthly_spending', 'any_drops', 'maths_results', 'hhh_literacy', 'age', 'gender', 'hhid', 'schoolid', 'group', 'group_cond', 'benef')
+
+X_vars <- c('hhh_gender', 'program_knowledge', 'hhh_age', 'monthly_spending', 'hhh_literacy', 'age', 'gender', 'benef')
+
 
 maths <- read_dta("LCT Morroco/cct_aser_an.dta")
 baseline <- read_dta("LCT Morroco/cct_baseline_an.dta")
@@ -15,7 +21,48 @@ endline <- read_dta("LCT Morroco/cct_endline_an.dta")
 school_baseline <- read_dta("LCT Morroco/cct_preliminary_survey_an.dta")
 dropout <- read_dta('LCT Morroco/cct_school_visits_an.dta')
 admin_data <- read_dta('LCT Morroco/cct_tayssir_admin_data_an.dta')
+knowledge <- read_dta("LCT Morroco/cct_knowledge_households_year1_an.dta")
 
+# Identify individual maths respondents
+maths_link <- endline %>%
+  filter(!is.na(hhid_endline) & hhid_endline != "") %>%
+  labelled::look_for_and_select("Child's ID") %>%
+  select(starts_with("d")) %>%
+  bind_cols(endline["hhid_endline"]) %>%
+  gather(key = "Variable", value = "Name", -hhid_endline) %>%
+  mutate(child_num = str_sub(Variable, -1, -1)) %>%
+  left_join(maths, by = "hhid_endline") %>%
+  filter(Name == id_enf_test)
+
+# Link to baseline data
+# Get the child ids in baseline
+selected <- baseline %>%
+  labelled::look_for_and_select("Member ID") %>%
+  select(starts_with("d")) %>%
+  bind_cols(baseline["hhid"]) %>%
+  gather(key = "Variable", value = "Name", -hhid) %>%
+  mutate(child_num = str_sub(Variable, -1, -1)) %>%
+  left_join(maths_link, by = c("hhid", "Name")) %>%
+  filter(Name == id_enf_test) %>%
+  select(hhid, child_num.x, Name) %>%
+  mutate(child_num.x = as.numeric(child_num.x))
+
+names(selected) <- c("hhid", "child_num", "member_id")
+baseline1 <- left_join(selected, baseline, by = "hhid")
+
+# Get section a self
+test1 <- baseline1 %>%
+  get_vars_by_regex("^a", "_[:digit:]+", "member_id") %>%
+  get_vars_by_regex("^c[[:digit:]]", "[:digit:]+", "child_num") %>%
+  get_vars_by_regex("^d[[:digit:]]", "_[:digit:]+", "child_num") %>%
+  get_vars_by_regex("^e[[:digit:]]", "_[:digit:]+", "child_num")
+
+
+
+# Check that DOBs roughly match up
+
+
+# Dropout outcomes
 dropouts <- dropout %>% 
   select(ends_with('_c12'))
 
@@ -25,9 +72,9 @@ dropouts3 <- dropouts2 > 0
 dropout$any_drops <- dropouts3
 
 joined <- baseline %>%
-  left_join(endline, by = "hhid", suffix = c("", ".y")) %>%
+  # left_join(endline, by = "hhid", suffix = c("", ".y")) %>%
   left_join(maths, by = "hhid", suffix = c("", ".y")) %>%
-  left_join(school_baseline, by = "schoolid", suffix = c("", ".y")) %>%
+  # left_join(school_baseline, by = "schoolid", suffix = c("", ".y")) %>%
   select_at(
     vars(-ends_with(".y"))
   )
@@ -37,6 +84,9 @@ baseline %<>%
          group = car::recode(group, "0 = NA;"),
          group_cond = car::recode(group, "1=1; else=2")
   )
+
+
+# Get variables about child at endline
 
 
 
@@ -106,22 +156,33 @@ assets[assets==-99]<-NA
 
 maths_results <- rowSums(maths[c('t6', 't5_1', 't4_1', 't3_1')])
 
+knowledge1 <- knowledge %>%
+  transmute(
+    hhid = hhid,
+    program_knowledge = ksm_c4_1 == 6
+  )
 
-
-
-joined1 <- baseline[c('hhid', 'group', 'group_cond', 'monthly_spending')] %>%
+joined1 <- baseline[c('hhid', 'group', 'group_cond', 'monthly_spending', 'schoolid')] %>%
   left_join(num_kids, by = "hhid") %>%
   left_join(temp, by = "hhid") %>%
+  left_join(knowledge1, by = "hhid") %>%
   filter(!duplicated(hhid))
 
 joined2 <- baseline %>%
   left_join(num_kids, by = "hhid") %>%
   left_join(temp, by = "hhid") %>%
+  left_join(knowledge1, by = "hhid") %>%
   filter(!duplicated(hhid))
 
 # joined1$PCA <- pca_results@scores %>% c()
 
-maths$maths_results <- rowSums(maths[c('t6', 't5_1', 't4_1', 't3_1')])
+maths %<>%
+  transmute(maths_results = as.numeric(t6 == 1) +
+           (t5_2 / 2) +
+           (t4_2 / 5) +
+           (t3_2 / 5),
+           hhid = hhid
+         )
 
 joined1 %<>%
   left_join(maths, by = "hhid")
@@ -146,35 +207,32 @@ joined2 %<>%
     vars(-ends_with(".y"))
   )
 
-covariates <- c('hhh_gender', 'hhh_age', 'monthly_spending', 'any_drops', 'hhh_literacy', 'age', 'gender', 'hhid', 'group', 'group_cond', 'benef')
 
-X_vars <- c('hhh_gender', 'hhh_age', 'monthly_spending', 'hhh_literacy', 'age', 'gender', 'benef')
-
-joined1 %<>%
-  dplyr::rename(hhh_age = a13,
-                hhh_gender = a4,
-                hhh_literacy = a15,
-                age = datenaiseleve,
-                gender = genre_el,
-                group = group.x)
-
-joined1 <- joined1[covariates]
+# joined1 %<>%
+#   dplyr::rename(hhh_age = a13,
+#                 hhh_gender = a4,
+#                 hhh_literacy = a15,
+#                 age = datenaiseleve,
+#                 gender = genre_el) %>%
+#   mutate(schoolid = as.factor(schoolid))
+# 
+# joined1 <- joined1[covariates]
 
 
 
-joined_num <- joined1 %>%
-  #   mutate(cluster = as.factor(hhid)) %>%
-  mutate(gender = factor(gender),
-         hhid = as.factor(hhid),
-         group = factor(group),
-         benef = factor(benef)
-  ) %>%
-  mutate(age = difftime(as.Date("2010-01-01"), age)) %>%
-  mutate_at(vars(-hhid), as.numeric) %>%
-  mutate(any_drops = (any_drops - 1) * -1)
-
-joined_num[joined_num < 0] <- NA
-joined_num <- joined_num[complete.cases(joined_num),]
+# joined_num <- joined1 %>%
+#   #   mutate(cluster = as.factor(hhid)) %>%
+#   mutate(gender = factor(gender),
+#          hhid = as.factor(hhid),
+#          group = factor(group),
+#          benef = factor(benef)
+#   ) %>%
+#   mutate(age = difftime(as.Date("2010-01-01"), age)) %>%
+#   mutate_at(vars(-hhid), as.numeric) %>%
+#   mutate(any_drops = (any_drops - 1) * -1)
+# 
+# joined_num[joined_num < 0] <- NA
+# # joined_num <- joined_num[complete.cases(joined_num),]
 
 
 joined_num2 <- joined2 %>%
@@ -182,9 +240,7 @@ dplyr::rename(hhh_age = a13,
               hhh_gender = a4,
               hhh_literacy = a15,
               age = datenaiseleve,
-              gender = genre_el,
-              group = group.x,
-              benef = benef.x) %>%
+              gender = genre_el) %>%
   #   mutate(cluster = as.factor(hhid)) %>%
   mutate(gender = factor(gender),
          hhid = as.factor(hhid),
@@ -198,7 +254,21 @@ dplyr::rename(hhh_age = a13,
 joined_num2[joined_num2 < 0] <- NA
 
 
-X <- joined_num[X_vars]
+# X <- joined_num[X_vars]
 
-joined_num %>% write_rds('morocco.rds')
+joined_num2 <- joined_num2[complete.cases(joined_num2[c("group", "maths_results")]),]
+# joined_num <- joined_num[complete.cases(joined_num[c("group", "maths_results")]),]
+
+# names(joined_num2) <- names(joined_num2) %>% str_replace_all(".x$", "")
+# names(joined_num) <- names(joined_num) %>% str_replace_all(".x$", "")
+# 
+
+# joined_num %>% write_rds('morocco.rds')
+
+# We've got duplicates in the data due to changes in household by baseline,
+# I use the first response
+
+
+
 joined_num2 %>% write_rds('morocco2.rds')
+
---
+++ helpers.R
@@ -0,0 +1,162 @@
+library(policytree)
+library(labelled)
+library(fuzzyjoin)
+
+
+impute_covars <- function(X) {
+  X_imp <- X %>%
+    missMethods::impute_median()
+  
+  X_imp[, colSums(is.na(X_imp)) < nrow(X_imp), drop = FALSE]
+}
+
+
+create_cowplot <- function(plot_data, variable_list, tau1) {
+  if(!is.data.frame(tau1)) tau1 <- as.data.frame(tau1)
+  
+  num_treats <- ncol(tau1)
+  plots <- vector(mode = "list", length = num_treats * length(variable_list))
+  
+  # Use an inner and outer map to map over variables then treatments within variables
+  new_plots <- map(
+    variable_list, 
+    function(variable) {
+      # Set number of options in question for later use
+      num_options <- length(unique(pull(plot_data[variable])))
+      map(tau1, function(tau) {
+        if (num_options > 10) {
+          ggplot(data = plot_data, aes(x=!!rlang::sym(variable), y = tau)) +
+            geom_jitter(alpha = 0.5) +
+            geom_smooth(color = "red") +
+            geom_hline(yintercept = 0, color = "blue", linetype = "dashed") +
+            xlab(variable)
+          
+        } else {
+          ggplot(data = plot_data, aes(x= !!rlang::sym(variable), y = tau)) +
+            geom_jitter(alpha = 0.5) +
+            geom_violin(aes(group = !!rlang::sym(variable)), alpha = 0.25) +
+            stat_summary(fun = "mean",
+                         geom = "point",
+                         color = "red",
+                         size = 6) +
+            geom_hline(yintercept = 0, color = "blue", linetype = "dashed") +
+            xlab(variable)
+        }
+      })
+    }
+  )
+  
+
+  plot_grid(plotlist = new_plots %>% unlist(recursive = F), 
+            ncol = num_treats)
+}
+
+
+qinish <- function(forest, treat_costs, learning_vars = NULL) {
+  # Get net benefit (benefit minus cost)
+  treat_tau <- forest$predictions %>% as.data.frame()
+  
+  dfs <- map(treat_tau, function(tau) {
+    if (!is.vector(treat_tau)) treat_tau <- pull(treat_tau)
+    if (length(treat_costs) == 1) treat_costs <- rep.int(treat_costs, length(treat_tau))
+    cb_ratio = treat_costs / treat_tau
+    cb_ratio[cb_ratio < 0] <- Inf
+    df <- data.frame(cb_point = cb_ratio, port_treat = 0)
+    for (i in 1:length(cb_ratio)) {
+      selector <- cb_ratio[i] <= cb_ratio
+      df[i, "port_treat"] <- sum(selector) / length(cb_ratio)
+    }
+    df
+  })
+  
+  names(dfs) <- paste0("treat", ncol(treat_tau))
+  
+  first_degree <- bind_rows(dfs, .id = "treatment")
+  
+  # Look at policy learner
+  
+  # Get covariates
+  if (is.null(learning_vars)) {
+    learning_vars <- forest$X.orig
+  }
+  
+  # Get DR scores
+  dr_scores <- double_robust_scores(forest)
+  
+  # Rerun analysis for learnt policy
+  first_degree %>%
+    mutate(
+      pct_treated = hybrid_policy_tree(
+        learning_vars %>% impute_covars(),
+        dr_scores[,c(1, as.numeric(treatment) + 1)],
+        search.depth = 1
+      ) %>%
+        predict(learning_vars %>% impute_covars()) %>%
+        as.tibble() %>%
+        mutate(dr = dr_scores[,c(1, as.numeric(treatment) + 1)]) %>%
+        filter(value == 2) %>%
+        nrow()
+        ,
+      pct_treated1 = pct_treated / nrow(treat_tau)
+    )
+}
+
+
+
+# Join maths to baseline based on names
+join_on_names <- function(baseline, endline, maths) {
+
+  maths$hhid_endline <- maths$hhid_endline %>%
+    as_factor() %>%
+    as.character()
+  maths %>%
+    filter(!is.na(hhid_endline) & hhid_endline != "")
+  
+  endline$hhid_endline <- endline$hhid_endline %>%
+    as_factor() %>%
+    as.character()
+  
+  baseline$endline
+  
+  out_df <- endline %>%
+    filter(!is.na(hhid_endline) & hhid_endline != "") %>%
+    labelled::look_for_and_select("Child's first name") %>%
+    bind_cols(endline["hhid_endline"]) %>%
+    gather(key = "Variable", value = "Name", -hhid_endline) %>%
+    left_join(maths, by = "hhid_endline") %>%
+    group_by(hhid_endline) %>%
+    mutate(str_dist = stringdist::stringdist(prenom_enf_test, Name, method = "lv") / nchar(prenom_enf_test)) %>%
+    arrange(str_dist) %>%
+    slice(1) %>%
+    ungroup()
+    
+  out_df_filtered <- out_df %>%  
+    filter(str_dist <= 0.25)
+  
+  if (nrow(out_df) != nrow(out_df_filtered)) {
+    warning(paste0("Some rows did not have a valid match at threshold of 0.25. ",
+                   nrow(out_df) - nrow(out_df_filtered),
+                   " rows did not have a match."))
+  }
+  
+  out_df_filtered
+  
+}
+
+
+get_vars_by_regex <- function(df, selector, num_selector, person_num) {
+  df[paste0(person_num, "join")] <- df[person_num]
+  
+  df %>%
+    select(hhid, matches(selector)) %>%
+    gather("name", "value", -hhid) %>%
+    transmute(value = value, 
+              person = str_extract(name, num_selector) %>% str_extract("[:digit:]+") %>% as.numeric(),
+              variable = str_replace(name, num_selector, ""),
+              variable = paste0(variable, "_self"),
+              hhid = hhid
+              ) %>%
+    pivot_wider(names_from = variable, values_from = value) %>%
+    right_join(df, by = c("hhid", "person" = paste0(person_num, "join")))
+}
+
